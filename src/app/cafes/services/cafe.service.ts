7a85d218f08ef67f245c4593b00e8ee88e652460
Robert Castro <ra.castro2@uniandes.edu.co>
2023-11-04 20:09:54 -0500

    refactor: Se agrega environment

--- src/app/cafes/services/cafe.service.ts
+++ src/app/cafes/services/cafe.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Cafe } from '../models/cafe';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CafeService {
 
-  private readonly URL = 'https://gist.githubusercontent.com/josejbocanegra/e9d24db370ce95b75555f7d1f8691805/raw/8a26ac2bca4183dc88545e14c45851d698911358/202212_MISW4104_Grupo3.json';
+  private readonly URL = environment.baseUrl;
   constructor(private http: HttpClient) { }
 
   getCafes(): Observable<Cafe[]> {
